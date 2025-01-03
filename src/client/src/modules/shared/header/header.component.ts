fa062027e333f4691f0cd7e44971a7e75569fc47
Tien Phong <tienphong2203@gmail.com>
2025-01-03 16:06:28 +0700

    FE: Implement view user header profile

--- src/client/src/modules/shared/header/header.component.ts
+++ src/client/src/modules/shared/header/header.component.ts
@@ -3,11 +3,13 @@ import { Component, Inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AUTH_SERVICE } from '../../../constants/injection.constant';
 import { IAuthService } from '../../../services/auth/auth-service.interface';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
@@ -16,6 +18,9 @@ export class HeaderComponent {
   public isAuthenticated: boolean = false;
 
   public userInformation: any;
+
+  public faUser: IconDefinition = faUser;
+  public isShow: boolean = false;
 
   constructor(@Inject(AUTH_SERVICE) private authService: IAuthService) {
     this.authService.isAuthenticated().subscribe((res) => {
@@ -28,6 +33,18 @@ export class HeaderComponent {
 
   public logout(): void {
     this.authService.logout();
+  }
+
+  public onShow(): void {
+    setTimeout(() => {
+      this.isShow = true;
+    }, 200);
+  }
+
+  public onHide(): void {
+    setTimeout(() => {
+      this.isShow = false;
+    }, 200);
   }
 
 }
