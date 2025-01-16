db800bbe046d020507adc483f4cc2935ac8c2aa9
Tien Phong <tienphong2203@gmail.com>
2025-01-16 11:10:31 +0700

    FE: Implement core promotion

--- src/client/src/modules/customer/promotion/promotion.component.ts
+++ src/client/src/modules/customer/promotion/promotion.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { PromotionCoreService } from '../../../services/promotion/promotion-core.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-promotion',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './promotion.component.html',
   styleUrl: './promotion.component.css'
 })
 export class PromotionComponent {
 
+  public promotionCore: any[] = [];
+
+  constructor(private promotionService: PromotionCoreService) {
+    this.promotionCore = this.promotionService.getPromotion();
+  }
 }
