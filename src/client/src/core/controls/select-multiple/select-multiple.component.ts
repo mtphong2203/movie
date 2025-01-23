5ef1fa155c9eeed7e47872da860d67aa0dcc147d
Tien Phong <tienphong2203@gmail.com>
2025-01-23 15:52:43 +0700

    FE: Implement user searching phone and multi select gender

--- src/client/src/core/controls/select-multiple/select-multiple.component.ts
+++ src/client/src/core/controls/select-multiple/select-multiple.component.ts
@@ -1,72 +1,98 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, HostListener, Output, ViewChild } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, forwardRef, HostListener, Input } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faAngleDown, faTimes, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 type Option = {
   id: number;
   name: string;
 }
-
 @Component({
   selector: 'app-select-multiple',
   standalone: true,
   imports: [CommonModule, FormsModule, FontAwesomeModule],
   templateUrl: './select-multiple.component.html',
-  styleUrl: './select-multiple.component.css'
-})
-export class SelectMultipleComponent {
-  public faTimes: IconDefinition = faTimes;
-  public faAngleDown: IconDefinition = faAngleDown;
-  public isShowOptions: boolean = false;
-  public keyword: string = '';
-  public options: Option[] = [
-    { id: 1, name: 'Admin' },
-    { id: 2, name: 'Manager' },
-    { id: 3, name: 'Customer' },
-    { id: 4, name: 'Staff' },
-    { id: 5, name: 'Partime Staff' },
+  styleUrl: './select-multiple.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectMultipleComponent),
+      multi: true
+    }
   ]
+})
+export class SelectMultipleComponent implements ControlValueAccessor {
+  @Input() options: any[] = [];
 
-  public filteredOptions: Option[] = this.options;
+  isShowOptions: boolean = false;
+  keyword: string = '';
+  filteredOptions: any[] = [];
+  selectedOptions: any[] = [];
 
-  @Output() public selectedOptions: Option[] = [];
+  private onChange: (value: any) => void = () => { };
+  private onTouched: () => void = () => { };
+  disabled: boolean = false;
 
-  public isSelected = (option: Option) => {
-    return this.selectedOptions.filter(selectedOption => selectedOption.id === option.id).length > 0;
+  ngOnInit(): void {
+    this.filteredOptions = this.options;
   }
 
-  public onSearch = (event: any) => {
-    this.keyword = event.target.value;
-    this.filteredOptions = this.options.filter(option => option.name.toLowerCase().includes(this.keyword.toLowerCase()));
-  }
-
-  public onSelect = (option: Option) => {
-    if (this.isSelected(option)) {
-      this.selectedOptions = this.selectedOptions.filter(selectedOption => selectedOption.id !== option.id);
-    } else {
-      this.selectedOptions.push(option);
-    }
-  }
-
-  public onRemove = (option: Option) => {
-    this.selectedOptions = this.selectedOptions.filter(selectedOption => selectedOption.id !== option.id);
-  }
-
-  public onToggle = () => {
-    this.isShowOptions = !this.isShowOptions;
-
-  }
-  // Using viewChild to get the element option container
-  @ViewChild('optionsContainer') optionsContainer!: ElementRef;
-
-  // Hidden options when click outside of the options container element
   @HostListener('document:click', ['$event'])
-  onDocumentClick(event: MouseEvent): void {
-    // If the click is outside the dropdown, do somehing
-    if (this.optionsContainer && !this.optionsContainer.nativeElement.contains(event.target)) {
+  handleClick(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.select-control')) {
       this.isShowOptions = false;
+      this.onTouched();
     }
+  }
+
+  onToggle() {
+    if (this.disabled) return;
+    this.isShowOptions = !this.isShowOptions;
+  }
+
+  onRemove(option: any) {
+    this.selectedOptions = this.selectedOptions.filter(item => item.id !== option.id);
+    this.onChange(this.selectedOptions.map(option => option.name));
+  }
+
+  onSearch(event: Event) {
+    const keyword = (event.target as HTMLInputElement).value.toLowerCase();
+    this.filteredOptions = this.options.filter(option =>
+      option.name.toLowerCase().includes(keyword)
+    );
+  }
+
+  onSelect(option: any) {
+    if (!this.isSelected(option) && !this.disabled) {
+      this.selectedOptions.push(option);
+      this.onChange(this.selectedOptions.map(option => option.name));
+    }
+    this.keyword = '';
+    this.filteredOptions = this.options;
+  }
+
+  isSelected(option: any): boolean {
+    return this.selectedOptions.some(item => item.id === option.id);
+  }
+
+  writeValue(value: any): void {
+    if (value) {
+      this.selectedOptions = this.options.filter(option => value.includes(option.name));
+    } else {
+      this.selectedOptions = [];
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
   }
 }
